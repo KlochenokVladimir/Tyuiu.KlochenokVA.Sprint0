3f007f663dda4ce2998b7950dcb32c5e2f5ba9ca
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-09-15 21:05:03 +0500

    Task6

---
+++ Tyuiu.KlochenokVA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KlochenokVA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность массива = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение массива = " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
